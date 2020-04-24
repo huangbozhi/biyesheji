2dcaa02dc44d84e7807d451748cda700a1ba08e0
huangbozhi <1044213015@qq.com>
2020-04-24 14:59:01 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style lang="less">
+@import "./style/common";
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
---
+++ src/components/login.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="login_page fillcontain">
+    <transition name="form-fade" mode="in-out">
+      <section class="form_contianer" v-show="true">
+        <div class="manage_tip">
+          <p>学历证明系统</p>
+        </div>
+        <el-form :model="loginForm" :rules="rules" ref="loginForm">
+          <el-form-item class="item" prop="username">
+            <el-input v-model="loginForm.username" placeholder="用户名">
+              <span>dsfsf</span>
+            </el-input>
+          </el-form-item>
+          <el-form-item class="item" prop="password">
+            <el-input type="password" placeholder="密码" v-model="loginForm.password"></el-input>
+          </el-form-item>
+          <el-form-item class="item">
+            <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">登陆</el-button>
+          </el-form-item>
+        </el-form>
+        <p class="tip">温馨提示：</p>
+        <p class="tip">未登录过的新用户，自动注册</p>
+        <p class="tip">注册过的用户可凭账号密码登录</p>
+      </section>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  data() {
+    return {
+      loginForm: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style  lang="less" scoped>
+@import "../style/mixin";
+.login_page {
+  background-color: #324057;
+}
+.manage_tip {
+  position: absolute;
+  width: 100%;
+  top: -100px;
+  left: 0;
+  p {
+    font-size: 34px;
+    color: #fff;
+  }
+}
+.form_contianer {
+  .wh(320px, 210px);
+  .ctp(320px, 210px);
+  padding: 25px;
+  border-radius: 5px;
+  text-align: center;
+  background-color: #fff;
+  .submit_btn {
+    width: 100%;
+    font-size: 16px;
+  }
+}
+.tip {
+  font-size: 12px;
+  color: red;
+}
+.form-fade-enter-active,
+.form-fade-leave-active {
+  transition: all 1s;
+}
+.form-fade-enter,
+.form-fade-leave-active {
+  transform: translate3d(0, -50px, 0);
+  opacity: 0;
+}
+.item {
+  // width: 40px;
+  height: 22px;
+}
+.el-form-item {
+  // border: 1px;
+  border: 1px solid #999;
+  margin-bottom: 5px;
+}
+.el-button {
+  margin-bottom: 25px;
+}
+</style>
